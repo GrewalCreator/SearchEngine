7d9f7fd9bda31f6784a5ccdbb0c6621103366cb9
Gurtej Grewal <gurtejgrewal455@gmail.com>
2026-03-06 18:25:22 -0500

    DB CRUD operations

---
+++ crawler/persistence/api.go
@@ -0,0 +1,183 @@
+package persistence
+
+import (
+	"errors"
+	"gorm.io/gorm"
+)
+
+func GetPage(db *gorm.DB, url string) (*Page, error) {
+
+	var page Page
+
+	// SELECT FIRST FROM Pages WHERE url = url
+	result := db.First(&page, "url = ?", url)
+	if result.Error != nil {
+		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+			return nil, nil
+		}
+
+		return nil, result.Error
+	}
+
+	return &page, nil
+}
+
+// CreatePage creates a new page entry in the database
+func CreatePage(db *gorm.DB, url string) (*Page, error) {
+
+	if url == "" {
+    	return nil, errors.New("url cannot be empty")
+	}
+
+	page := Page{
+		URL: url,
+	}
+
+	result := db.Create(&page)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	return &page, nil
+}
+
+
+/*
+* GetOrCreatePage Gets the page at url
+* If the page does not exist, it will create it and return the page
+*/
+func GetOrCreatePage(db *gorm.DB, url string) (*Page, error) {
+	if url == "" {
+		return nil, errors.New("url cannot be empty")
+	}
+
+	page := Page{URL: url}
+
+	result := db.Where("url = ?", url).FirstOrCreate(&page)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	return &page, nil
+}
+
+// UpdatePageTitle updates the title of page with ID pageID
+func UpdatePageTitle(db *gorm.DB, pageID uint, title string) error {
+	result := db.Model(&Page{}).
+		Where("id = ?", pageID).
+		Update("title", title)
+
+	return result.Error
+}
+
+
+// UpdatePageRank updates the pageRank of page with ID pageID
+func UpdatePageRank(db *gorm.DB, pageID uint, pageRank float64) error {
+	result := db.Model(&Page{}).
+		Where("id = ?", pageID).
+		Update("page_rank", pageRank)
+
+	return result.Error
+}
+
+
+// UpdatePage updates multiple values of page record
+func UpdatePage(db *gorm.DB, pageID uint, updates map[string] any) error {
+	result := db.Model(&Page{}).
+		Where("id = ?", pageID).
+		Updates(updates)
+
+	return result.Error
+}
+
+
+// DeletePage safely deletes a page and removes related links and word counts
+func DeletePage(db *gorm.DB, pageID uint) error {
+	tx := db.Begin()
+
+	if tx.Error != nil {
+		return tx.Error
+	}
+
+	if err := tx.Where("from_page_id = ? OR to_page_id = ?", pageID, pageID).Delete(&Link{}).Error; err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	if err := tx.Where("page_id = ?", pageID).Delete(&WordCount{}).Error; err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	if err := tx.Delete(&Page{}, pageID).Error; err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	return tx.Commit().Error
+}
+
+// CreateLink creates a link from page with Id fromID to page with ID toID
+func CreateLink(db *gorm.DB, fromID uint, toID uint) error {
+	link := Link{
+		FromPageID: fromID,
+		ToPageID:   toID,
+	}
+
+	result := db.Where("from_page_id = ? AND to_page_id = ?", fromID, toID).
+		FirstOrCreate(&link)
+
+	return result.Error
+}
+
+// Returns all outgoing links of page with ID pageID
+func GetOutgoingLinks(db *gorm.DB, pageID uint) ([]Link, error) {
+	var links []Link
+
+	result := db.Where("from_page_id = ?", pageID).Find(&links)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	return links, nil
+}
+
+// Returns all incoming links of page with ID pageID
+func GetIncomingLinks(db *gorm.DB, pageID uint) ([]Link, error) {
+	var links []Link
+
+	result := db.Where("to_page_id = ?", pageID).Find(&links)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	return links, nil
+}
+
+// Saves/Updates the word count map of page with ID pageID
+func SaveWordCounts(db *gorm.DB, pageID uint, counts map[string] uint) error {
+	tx := db.Begin()
+	if tx.Error != nil {
+		return tx.Error
+	}
+
+	if err := tx.Where("page_id = ?", pageID).Delete(&WordCount{}).Error; err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	for word, count := range counts {
+		wordCount := WordCount{
+			PageID: pageID,
+			Word:   word,
+			Count:  count,
+		}
+
+		if err := tx.Create(&wordCount).Error; err != nil {
+			tx.Rollback()
+			return err
+		}
+	}
+
+	return tx.Commit().Error
+}
